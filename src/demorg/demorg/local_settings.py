380cbcda2c8f4c189d2d50b04de1fb40801f05f5
Carlos-M-A <carlos.ma.trabajo@gmail.com>
2021-08-02 13:05:33 +0200

    feat(demorg): Basic project skeleton
    Add the basic project skeleton with the django structure. Add basic settings

---
+++ src/demorg/demorg/local_settings.py
@@ -0,0 +1,13 @@
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'demorg',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
